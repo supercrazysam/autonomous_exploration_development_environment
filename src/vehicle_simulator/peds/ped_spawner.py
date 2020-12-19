bf8f15eb18d4b6681a13f17d4f3d152123882e79
Liquid <sammy11257@gmail.com>
2020-12-18 22:22:47 -0500

    pedestrian prototype

--- src/vehicle_simulator/peds/ped_spawner.py
+++ src/vehicle_simulator/peds/ped_spawner.py
@@ -7,13 +7,15 @@ from geometry_msgs.msg import Pose
 rospy.init_node("insert_object")
 
 initial_pose = Pose()
-initial_pose.position.x = 1
-initial_pose.position.y = 1
-initial_pose.position.z = 1
+initial_pose.position.x = -6.96759986877 #1
+initial_pose.position.y = -57.2897491455 #1 
+initial_pose.position.z = 1 #1
 
 f = open('/home/big/catkin_ws/src/autonomous_exploration_development_environment/src/vehicle_simulator/peds/ped.sdf','r')
 sdff = f.read()
 
 rospy.wait_for_service('gazebo/spawn_sdf_model')
 spawn_model_prox = rospy.ServiceProxy('gazebo/spawn_sdf_model', SpawnModel)
-spawn_model_prox("some_robo_name", sdff, "robotos_name_space", initial_pose, "world")
+spawn_model_prox("ped_0", sdff, "ped_0_name_space", initial_pose, "world") #world
+
+
